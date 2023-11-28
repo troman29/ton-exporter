c2f2f46b52114ce9e873c01a2422407a8aa0b2cf
troman29 <mc-monstr@yandex.ru>
2023-11-28 16:04:21 +0300

    Add config.yaml; Add controllers and validator status

--- ton-exporter.py
+++ ton-exporter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-import asyncio
 import os
 import traceback
 from datetime import datetime as dt
+from typing import List, Optional
 
-import aiohttp
+import asyncio
+from aiohttp import ClientSession
+from pydantic import BaseModel
+from yaml import load, SafeLoader
 from prometheus_client import (
     start_http_server,
     Gauge,
@@ -14,78 +17,127 @@ from prometheus_client import (
     PROCESS_COLLECTOR,
 )
 
-HTTP_PORT = 9150
-INTERVAL = 30
-
-VALIDATOR_ADDRESSES = dict([x.split(':') for x in os.getenv('VALIDATOR_ADDRESSES').split(',')])
-POOL_ADDRESSES = dict([x.split(':') for x in os.getenv('POOL_ADDRESSES').split(',')])
-API_BASE_URL = 'https://toncenter.com/api/v2'
-X_API_KEY = os.getenv('TON_X_API_KEY')
-MIN_ELECTOR_TX_AMOUNT = 300000
-
 # Setup metrics
 REGISTRY.unregister(GC_COLLECTOR)
 REGISTRY.unregister(PLATFORM_COLLECTOR)
 REGISTRY.unregister(PROCESS_COLLECTOR)
 BALANCE = Gauge('address_balance', 'The balance of the address in the TON blockchain', ['address', 'name'])
 TIME_SINCE = Gauge('time_since_last_big_tx', 'Time since the last big transaction', ['address', 'name'])
+
+VALIDATOR_ACTIVE = Gauge('validator_active', 'Validator status (0 - inactive, 1 - active)', ['address', 'name'])
 POOL_STATUS = Gauge('pool_state', 'Pool status (0 - inactive, 1 - pending, 2 - active)', ['address', 'name'])
-DEPOSIT = Gauge('pool_deposit', 'Total deposit of nominators', ['address', 'name'])
+POOL_DEPOSIT = Gauge('pool_deposit', 'Total deposit of nominators', ['address', 'name'])
+CONTROLLER_STATUS = Gauge(
+    'controller_state',
+    'Controller status (0 - REST, 1 - SENT_BORROWING_REQUEST, 2 - SENT_STAKE_REQUEST,'
+    ' 3 - FUNDS_STAKEN, 4 - SENT_RECOVER_REQUEST, 5 - INSOLVENT)',
+    ['address', 'name']
+)
+
+# Settings
+HTTP_PORT = int(os.getenv('HTTP_PORT', 9150))
+TON_API_URL = 'https://toncenter.com/api/v2'
+ELECTIONS_API_URL = 'https://elections.toncenter.com'
+X_API_KEY = os.getenv('TON_X_API_KEY')
+
+MIN_ELECTOR_TX_AMOUNT = 300000
+TRANSACTIONS_LIMIT = 50
+INTERVAL = 300
+
+
+# Declare config
+class Config(BaseModel):
+    class Validator(BaseModel):
+        name: str
+        wallet: str
+        pools: Optional[List[str]] = None
+        controllers: Optional[List[str]] = None
+
+    validators: List[Validator]
+
+
+config: Config
+session: ClientSession
+active_validators: set[str]
 
 
 async def main():
+    global config
+    global session
+    global active_validators
+
+    # Parse config
+    with open('config.yaml') as file:
+        config_dict = load(file.read(), Loader=SafeLoader)
+
+    config = Config(**config_dict)
+    session = ClientSession()
+
     while True:
-        print('\nCollecting metrics...')
-        async with aiohttp.ClientSession() as session:
-            for name, address in POOL_ADDRESSES.items():
-                await collect_pool(session, name, address)
-            for name, address in VALIDATOR_ADDRESSES.items():
-                await collect_validator(session, name, address)
+        try:
+            print('\nCollecting metrics...')
+            active_validators = set(await get_active_validators())
+            await asyncio.gather(*map(collect_validator, config.validators))
+        except Exception:
+            traceback.print_exc()
         await asyncio.sleep(INTERVAL)
 
 
-async def collect_validator(session, name, address):
-    try:
-        async with session.get(
-            f'{API_BASE_URL}/getWalletInformation?address={address}',
-            headers={'X-Api-Key': X_API_KEY},
-        ) as response:
-            raw_balance = (await response.json())['result']['balance']
+async def collect_validator(validator: Config.Validator):
+    address = validator.wallet
+    name = validator.name
+    pools = validator.pools or []
+    controllers = validator.controllers or []
 
-        balance = round(int(raw_balance) / (10 ** 9))
+    try:
+        # update status
+        is_active = address in active_validators
+
+        if pools:
+            is_active = pools[0] in active_validators or pools[1] in active_validators
+        elif controllers:
+            is_active = controllers[0] in active_validators or controllers[1] in active_validators
+
+        CONTROLLER_STATUS.labels(address, name).set(int(is_active))
+
+        # update balance
+        balance = round(await get_balance(address))
         BALANCE.labels(address, name).set(balance)
-        print(f'{name}:', balance)
+
+        print(f'{name}:', balance, is_active)
     except Exception:
         traceback.print_exc()
-    await asyncio.sleep(0.2)
+
+    # Pools and controllers
+    validator_name = name.replace('-', '')
+
+    for i, pool in enumerate(pools or []):
+        name = f'{validator_name}-pool{i + 1}'
+        await collect_pool(name, pool)
+
+    for i, controller in enumerate(controllers or []):
+        name = f'{validator_name}-controller{i + 1}'
+        await collect_controller(name, controller)
 
 
-async def collect_pool(session, name, address):
+async def collect_pool(name: str, address: str):
+    global session
     try:
-        async with session.get(
-            f'{API_BASE_URL}/getTransactions?address={address}&limit=100&archival=true',
-            headers={'X-Api-Key': X_API_KEY},
-        ) as response:
-            response.raise_for_status()
-            txs = map(parse_raw_tx, (await response.json())['result'])
-            tx = next((x for x in txs if abs(x['amount']) > MIN_ELECTOR_TX_AMOUNT and not x['is_incoming']), None)
-            delta_s = int(
-                (dt.utcnow() - dt.utcfromtimestamp(tx['timestamp'])).total_seconds()
-            ) if tx else 259200
-            TIME_SINCE.labels(address, name).set(delta_s)
+        # Get transactions
+        txs = await get_transactions(address)
+        tx = next((x for x in txs if abs(x['amount']) > MIN_ELECTOR_TX_AMOUNT and not x['is_incoming']), None)
+        delta_s = int(
+            (dt.utcnow() - dt.utcfromtimestamp(tx['timestamp'])).total_seconds()
+        ) if tx else 259200
+        TIME_SINCE.labels(address, name).set(delta_s)
 
-        async with session.post(
-            f'{API_BASE_URL}/runGetMethod',
-            json={'address': address, 'method': 'get_pool_data', 'stack': []},
-            headers={'X-Api-Key': X_API_KEY},
-        ) as response:
-            response.raise_for_status()
-            stack = (await response.json())['result']['stack']
-            state = int(stack[0][1], base=16)
-            deposit = round(int(stack[2][1], base=16) / (10 ** 9))
+        # Run get method
+        stack = await run_get_method(address, 'get_pool_data')
+        state = int(stack[0][1], base=16)
+        deposit = round(int(stack[2][1], base=16) / (10 ** 9))
 
-            POOL_STATUS.labels(address, name).set(state)
-            DEPOSIT.labels(address, name).set(deposit)
+        POOL_STATUS.labels(address, name).set(state)
+        POOL_DEPOSIT.labels(address, name).set(deposit)
 
         print(
             f'{name}:',
@@ -98,6 +150,58 @@ async def collect_pool(session, name, address):
     except Exception:
         traceback.print_exc()
     await asyncio.sleep(0.2)
+
+
+async def collect_controller(name: str, address: str):
+    try:
+        stack = await run_get_method(address, 'get_validator_controller_data')
+        state = int(stack[0][1], base=16)
+        CONTROLLER_STATUS.labels(address, name).set(state)
+        print(f'{name}:', state)
+    except Exception:
+        traceback.print_exc()
+
+
+async def get_active_validators():
+    async with session.get(
+        f'{ELECTIONS_API_URL}/getValidationCycles?limit=1&return_participants=true',
+        headers={'X-Api-Key': X_API_KEY},
+    ) as response:
+        raw_validators = (await response.json())[0]['cycle_info']['validators']
+
+    return [x['wallet_address'] for x in raw_validators]
+
+
+async def run_get_method(address: str, method: str, stack: Optional[List[str]]=None) -> List[str]:
+    global session
+    async with session.post(
+        f'{TON_API_URL}/runGetMethod',
+        json={'address': address, 'method': method, 'stack': stack or []},
+        headers={'X-Api-Key': X_API_KEY},
+    ) as response:
+        response.raise_for_status()
+        return (await response.json())['result']['stack']
+
+
+async def get_balance(address: str):
+    global session
+    async with session.get(
+        f'{TON_API_URL}/getWalletInformation?address={address}',
+        headers={'X-Api-Key': X_API_KEY},
+    ) as response:
+        raw_balance = (await response.json())['result']['balance']
+    return int(raw_balance) / (10**9)
+
+
+async def get_transactions(address: str):
+    global session
+    async with session.get(
+        f'{TON_API_URL}/getTransactions?address={address}&limit={TRANSACTIONS_LIMIT}&archival=true',
+        headers={'X-Api-Key': X_API_KEY},
+    ) as response:
+        response.raise_for_status()
+        raw_txs = (await response.json())['result']
+        return map(parse_raw_tx, raw_txs)
 
 
 def parse_raw_tx(raw_tx: dict):
